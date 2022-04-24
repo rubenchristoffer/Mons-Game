eb431ebd942bac9496447b83ea135e2611e5e99e
Ruben Christoffer <rubenc@protonmail.com>
2022-04-24 22:38:53 +0200

    Scripts: Added MonsEntityEditor

---
+++ Assets/Scripts/Editor/MonsEntityEditor.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MonsEntity))]
+public class MonsEntityEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI();
+
+		var script = (MonsEntity) target;
+
+		if (!Application.isPlaying) return;
+		
+		GUILayout.Label($"Lives: {script.lives}");
+		GUILayout.Label($"Is Dead: {script.isDead}");
+	}
+
+}

dd08f505c43c94eb87c79b52012c83b2d555851f
Ruben Christoffer <rubenc@protonmail.com>
2022-04-24 21:24:42 +0200

    Scripts: Added custom editor for DistanceTracker

---
+++ Assets/Scripts/Editor/DistanceTrackerEditor.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(DistanceTracker))]
+public class DistanceTrackerEditor : Editor {
+
+	public override void OnInspectorGUI () {
+		base.OnInspectorGUI();
+
+		var script = (DistanceTracker) target;
+		
+		GUILayout.Label($"Horizontal Distance Travelled: {script.horizontalDistanceTravelled}");
+	}
+
+}

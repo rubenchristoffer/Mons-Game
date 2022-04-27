6b12ad0be56944c52f43b1040a0adf37db9502ee
Ruben Christoffer <rubenc@protonmail.com>
2022-04-27 22:10:49 +0200

    UI: Added Game Over dialogue

---
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverPanel : MonoBehaviour {
+
+	public const string MAIN_MENU_SCENE = "MainMenu";
+	
+	public void BtnOnRestartLevelPressed () {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	public void BtnOnMainMenuPressed () {
+		Time.timeScale = 1f;
+		SceneManager.LoadScene(MAIN_MENU_SCENE);
+	}
+	
+	void Awake () {
+		Object.FindObjectOfType<LevelState>().eventFailedLevel.AddListener(LevelState_OnFailedLevel);
+		
+		gameObject.SetActive(false);
+	}
+
+	void LevelState_OnFailedLevel () {
+		gameObject.SetActive(true);
+		Time.timeScale = 0f;
+	}
+	
+}
